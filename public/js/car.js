fe417dd5edd5b34a98206309ad6417d5a43818ea
George Moschovitis <gmosx@reizu.com>
2013-09-06 15:29:20 +0300

    Car movement.

--- public/js/car.js
+++ public/js/car.js
@@ -6,13 +6,13 @@ var Car = function (params) {
     this.$host = $('<div class="car"></div>');
     this.x = undefined;
     this.y = undefined;
-    this.angle = 180; // angle in degrees.
+    this.angle = 0; // angle in degrees.
 };
 
 Car.prototype.update = function () {
     this.$host.css({
-       top: this.x,
-       left: this.y,
+       top: this.y,
+       left: this.x,
        transform: 'rotate(' + this.angle + 'deg)'
     });
 };
@@ -23,6 +23,21 @@ Car.prototype.append = function ($container) {
 };
 
 Car.prototype.remove = function () {
+};
+
+//Object.defineProperties(Car.prototype, {
+//    'delta': {
+//        get: function () {
+//            return (this.angle / 180.0) * Math.PI;
+//        }
+//    }
+//});
+
+Car.prototype.move = function (delta) {
+    var radians = (this.angle / 180.0) * Math.PI;
+
+    this.y += delta * Math.sin(radians);
+    this.x += delta * Math.cos(radians);
 };
 
 exports.Car = Car;
--- public/js/main.js
+++ public/js/main.js
@@ -17,26 +17,60 @@ ViewApp.prototype.start = function () {
 //        correctLevel : QRCode.CorrectLevel.H
     });
 
-    this.connect();
+    this.$playfield = $(document.body); // TODO: temp!
 
-    var $playfield = $(document.body);
+    this._bindEvents();
+    this._connect();
 
-    var car = new Car();
-    car.x = 100;
-    car.y = 200;
+    this.car = new Car();
+    this.car.x = 100;
+    this.car.y = 200;
 
-    car.append($playfield);
+    this.car.append(this.$playfield);
 };
 
-ViewApp.prototype.bindEvents = function () {
+ViewApp.prototype._bindEvents = function () {
+    this.$playfield.on('keydown', this._onKeyDown.bind(this));
 };
 
-ViewApp.prototype.connect = function () {
+ViewApp.prototype._connect = function () {
     var socket = io.connect('http://localhost:8000');
 
     socket.on('connect', function (data) {
         console.log('connected to server:' + data);
     });
+};
+
+ViewApp.prototype._onKeyDown = function (e) {
+    var handled = false;
+
+    switch (e.keyCode) {
+        case 38: // UP
+            this.car.move(5);
+            handled = true;
+            break;
+
+        case 40: // DOWN
+            this.car.move(-5);
+            handled = true;
+            break;
+
+        case 39: // LEFT
+            this.car.angle += 1;
+            handled = true;
+            break;
+
+        case 37: // RIGHT
+            this.car.angle -= 1;
+            handled = true;
+            break;
+    }
+
+    if (handled) {
+        this.car.update();
+        e.preventDefault();
+        e.stopPropagation();
+    }
 };
 
 exports.ViewApp = ViewApp;
