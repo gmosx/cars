2061d7779f301c2595d4bbf037c54dfa9ff90c8b
Karabur <ant@aa.aa>
2013-09-06 13:55:07 +0300

    more from me

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,15 @@ var ViewApp = function (params) {
 };
 
 ViewApp.prototype.start = function () {
+    new QRCode(document.getElementById("qrCode"), {
+        text: window.location.host+'/player.html',
+        width: 128,
+        height: 128,
+        colorDark : "#000000",
+        colorLight : "#ffffff"//,
+//        correctLevel : QRCode.CorrectLevel.H
+    });
+
     var socket = io.connect('http://localhost:8000');
 
     socket.on('connect', function (data) {
