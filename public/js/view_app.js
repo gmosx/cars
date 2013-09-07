e40c07d4037451203eb4d1e79ead60954ae9c948
George Moschovitis <gmosx@reizu.com>
2013-09-07 14:21:46 +0300

    Minor.

--- public/js/view_app.js
+++ public/js/view_app.js
@@ -57,7 +57,6 @@ ViewApp.prototype._onKillPlayer = function (data) {
     var player = this.players[data.id];
     console.log(data.id);
     if (player) {
-        console.log('---1');
         player.remove();
         this.removeRacer(player);
         delete this.players[data.id];
